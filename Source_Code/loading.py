867a9f4e746497d0a275f6ee668f0b4da0c380ae
Mayur-Debu <iammayur.515@gmail.com>
2021-05-02 22:11:18 +0530

    Detect Malware module optimized

--- Source_Code/loading.py
+++ Source_Code/loading.py
@@ -1,110 +1,147 @@
+"""
+This python file contains all the function with all the intended loading animation to be loaded when requested.
+"""
 import time
 import sys
 
-""" This python file contains the loading animation. """
+
 
 
 def animateDecrypting():
+    ''' 
+    This function is intended to show the loading animation shown in the tool while loading anything.
+    '''
+
     percentage = 0
     for _ in range(1):
         # for cha in character:
         percentage = percentage + 10
-        sys.stdout.write(f'\r Decrypting ████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Decrypting ████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ {percentage}')
         time.sleep(.1)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Decrypting ████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Decrypting ████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ {percentage}')
         time.sleep(.1)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Decrypting ████████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Decrypting ████████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░ {percentage}')
         time.sleep(.1)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Decrypting ████████████████░░░░░░░░░░░░░░░░░░░░░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Decrypting ████████████████░░░░░░░░░░░░░░░░░░░░░░░░ {percentage}')
         time.sleep(.1)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Decrypting ████████████████████░░░░░░░░░░░░░░░░░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Decrypting ████████████████████░░░░░░░░░░░░░░░░░░░░ {percentage}')
         time.sleep(.1)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Decrypting ████████████████████████░░░░░░░░░░░░░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Decrypting ████████████████████████░░░░░░░░░░░░░░░░ {percentage}')
         time.sleep(.1)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Decrypting ████████████████████████████░░░░░░░░░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Decrypting ████████████████████████████░░░░░░░░░░░░ {percentage}')
         time.sleep(.1)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Decrypting ████████████████████████████████░░░░░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Decrypting ████████████████████████████████░░░░░░░░ {percentage}')
         time.sleep(.1)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Decrypting ████████████████████████████████████░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Decrypting ████████████████████████████████████░░░░ {percentage}')
         time.sleep(.1)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Decrypting ████████████████████████████████████████ {percentage}')
+        sys.stdout.write(
+            f'\r Decrypting ████████████████████████████████████████ {percentage}')
         time.sleep(.1)
 
 
 def initializingNetstat():
     percentage = 0
     for _ in range(1):
-        sys.stdout.write(f'\r Initializing Netstat ████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ ')
+        sys.stdout.write(
+            f'\r Initializing Netstat ████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Initializing Netstat ████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ ')
+        sys.stdout.write(
+            f'\r Initializing Netstat ████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Initializing Netstat ████████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░ ')
+        sys.stdout.write(
+            f'\r Initializing Netstat ████████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Initializing Netstat ████████████████░░░░░░░░░░░░░░░░░░░░░░░░ ')
+        sys.stdout.write(
+            f'\r Initializing Netstat ████████████████░░░░░░░░░░░░░░░░░░░░░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Initializing Netstat ████████████████████░░░░░░░░░░░░░░░░░░░░ ')
+        sys.stdout.write(
+            f'\r Initializing Netstat ████████████████████░░░░░░░░░░░░░░░░░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Initializing Netstat ████████████████████████░░░░░░░░░░░░░░░░ ')
+        sys.stdout.write(
+            f'\r Initializing Netstat ████████████████████████░░░░░░░░░░░░░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Initializing Netstat ████████████████████████████░░░░░░░░░░░░ ')
+        sys.stdout.write(
+            f'\r Initializing Netstat ████████████████████████████░░░░░░░░░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Initializing Netstat ████████████████████████████████░░░░░░░░ ')
+        sys.stdout.write(
+            f'\r Initializing Netstat ████████████████████████████████░░░░░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Initializing Netstat ████████████████████████████████████░░░░ ')
+        sys.stdout.write(
+            f'\r Initializing Netstat ████████████████████████████████████░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Initializing Netstat ████████████████████████████████████████ ')
+        sys.stdout.write(
+            f'\r Initializing Netstat ████████████████████████████████████████ ')
         time.sleep(.5)
 
 
 def netstatPortNumber():
     percentage = 0
     for _ in range(1):
-        sys.stdout.write(f'\r Extracting Port ID ████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ ')
+        sys.stdout.write(
+            f'\r Extracting Port ID ████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Extracting Port ID ████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ ')
+        sys.stdout.write(
+            f'\r Extracting Port ID ████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Extracting Port ID ████████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░ ')
+        sys.stdout.write(
+            f'\r Extracting Port ID ████████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Extracting Port ID ████████████████░░░░░░░░░░░░░░░░░░░░░░░░ ')
+        sys.stdout.write(
+            f'\r Extracting Port ID ████████████████░░░░░░░░░░░░░░░░░░░░░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Extracting Port ID ████████████████████░░░░░░░░░░░░░░░░░░░░ ')
+        sys.stdout.write(
+            f'\r Extracting Port ID ████████████████████░░░░░░░░░░░░░░░░░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Extracting Port ID ████████████████████████░░░░░░░░░░░░░░░░ ')
+        sys.stdout.write(
+            f'\r Extracting Port ID ████████████████████████░░░░░░░░░░░░░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Extracting Port ID ████████████████████████████░░░░░░░░░░░░ ')
+        sys.stdout.write(
+            f'\r Extracting Port ID ████████████████████████████░░░░░░░░░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Extracting Port ID ████████████████████████████████░░░░░░░░ ')
+        sys.stdout.write(
+            f'\r Extracting Port ID ████████████████████████████████░░░░░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Extracting Port ID ████████████████████████████████████░░░░ ')
+        sys.stdout.write(
+            f'\r Extracting Port ID ████████████████████████████████████░░░░ ')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Extracting Port ID ████████████████████████████████████████ ')
+        sys.stdout.write(
+            f'\r Extracting Port ID ████████████████████████████████████████ ')
         time.sleep(.5)
 
 
@@ -113,33 +150,42 @@ def scripLoading():
     for _ in range(1):
         # for cha in character:
         percentage = percentage + 10
-        sys.stdout.write(f'\r Loading Your Script ████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Loading Your Script ████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ {percentage}')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Loading Your Script ████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Loading Your Script ████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ {percentage}')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Loading Your Script ████████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Loading Your Script ████████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░ {percentage}')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Loading Your Script ████████████████░░░░░░░░░░░░░░░░░░░░░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Loading Your Script ████████████████░░░░░░░░░░░░░░░░░░░░░░░░ {percentage}')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Loading Your Script ████████████████████░░░░░░░░░░░░░░░░░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Loading Your Script ████████████████████░░░░░░░░░░░░░░░░░░░░ {percentage}')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Loading Your Script ████████████████████████░░░░░░░░░░░░░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Loading Your Script ████████████████████████░░░░░░░░░░░░░░░░ {percentage}')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Loading Your Script ████████████████████████████░░░░░░░░░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Loading Your Script ████████████████████████████░░░░░░░░░░░░ {percentage}')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Loading Your Script ████████████████████████████████░░░░░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Loading Your Script ████████████████████████████████░░░░░░░░ {percentage}')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Loading Your Script ████████████████████████████████████░░░░ {percentage}')
+        sys.stdout.write(
+            f'\r Loading Your Script ████████████████████████████████████░░░░ {percentage}')
         time.sleep(.5)
         percentage = percentage + 10
-        sys.stdout.write(f'\r Loading Your Script ████████████████████████████████████████ {percentage}')
+        sys.stdout.write(
+            f'\r Loading Your Script ████████████████████████████████████████ {percentage}')
         time.sleep(.5)
-
