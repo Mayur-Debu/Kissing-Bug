e4ea8a05047b261af254ddfa96e61649eefe607a
Mayur-Debu <iammayur.515@gmail.com>
2021-05-02 17:36:01 +0530

    Testing phase 1 for version 1

--- initial.py
+++ initial.py
@@ -1,27 +1,29 @@
-from win10toast import ToastNotifier
+# from win10toast import ToastNotifier
 import random
 from termcolor import cprint, colored
 import os
 import prettytable as pt
 
+# clearing the content on powershell before executing
 os.system('cls')
 
-# Generating a pretty table.
-sentence = colored('Make sure, you run this application as administrator.', 'cyan', attrs=['bold'])
-width = 127
-t = pt.PrettyTable()
-t.field_names = [colored('NOTE', 'red', attrs=['bold'])]
-[t.add_row([sentence[i:i + width]]) for i in range(0, len(sentence), width)]
-print(t)
-cprint(f'ENTER THE SOFTWARE ACTIVATION CODE SENT TO : ', 'red', attrs=['bold'])
+# printing the instructions on the screen
+instructionOnStartup = colored("Turn off Windows Defender and run as Administrator", 'cyan', attrs=['bold'])
+widthOfTable = 127
+tableObject = pt.PrettyTable()
+tableObject.field_names = [colored("Instruction to the user", 'red', attrs=['bold'])]
+[tableObject.add_row([instructionOnStartup[i:i + widthOfTable]]) for i in range(0, len(instructionOnStartup), widthOfTable)]
+print(tableObject)
 
-# Toast Notification Sent to your desktop.
-toast = ToastNotifier()
+# sending the desktop notification to the user
+notification = ToastNotifier()
 activation_code = random.randint(1000, 15000)
-toast.show_toast('KISSING BUG', f' Your Activation Code is ' + str(activation_code), icon_path='bug.ico')
+notification.show_toast("KISSING BUG", f"Your software activation code is: " + str(activation_code), icon_path='bug.ico')
 
+cprint(f"Enter the software activation code: ", 'red', attrs=['bold'])
 user_input = input()
 
+# authentication and authorized access
 if int(user_input) == int(activation_code):
     os.system('cls')
     os.chdir("Source_Code")
@@ -30,6 +32,7 @@ else:
     cprint('Resend? (y/n): ', 'cyan', attrs=['bold'])
     choice = input()
     if choice == 'y' or choice == 'Y':
+        os.system('cls')
         os.system('python initial.py')
     else:
         exit()
